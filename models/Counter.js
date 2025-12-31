bba67c3c60d18726a05f163af64a09164142319e
gokulram1818 <ggokulaparna@gmail.com>
2025-12-31 21:44:01 +0530

    Update Counter.js

--- models/Counter.js
+++ models/Counter.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const counterSchema = new mongoose.Schema({
-  _id: { type: String, required: true }, // collection name
+  _id: { type: String, required: true },
   seq: { type: Number, default: 0 },
 });
 
