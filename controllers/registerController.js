35f834ca75f05c24cd26ab1a276136c5db8fb5e6
Gokul Ram <ggokulaparna@gmail.com>
2026-01-02 03:22:35 +0530

    UI update

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,5 +1,6 @@
 import Registration from "../models/Registration.js";
 import getNextId from "../utils/getNextId.js";
+import Counter from "../models/Counter.js";
 
 export const registerUser = async (req, res) => {
   try {
@@ -14,7 +15,6 @@ export const registerUser = async (req, res) => {
       events,
     } = req.body;
 
-    
     if (
       !participant1Name ||
       !participant1Roll ||
@@ -27,10 +27,41 @@ export const registerUser = async (req, res) => {
       return res.status(400).json({ message: "Required fields missing" });
     }
 
+
     
     const regId = await getNextId("registration");
 
-    
+    const eventLimits = {
+      promptMasters: 50,
+      codeRelay: 50,
+    };
+
+    for (const event in eventLimits) {
+      if (events[event]) {
+        const counter = await Counter.findById(event);
+
+        if (!counter) {
+          return res
+            .status(500)
+            .json({ message: `${event} counter not initialized` });
+        }
+
+        if (counter.seq >= eventLimits[event]) {
+          return res.status(400).json({
+            message:
+              event === "promptMasters"
+                ? "Prompt Masters registrations are full"
+                : "Code Relay registrations are full",
+          });
+        }
+
+        counter.seq += 1;
+        await counter.save();
+      }
+    }
+
+    const regId = await getNextId("registration");
+
     const registration = await Registration.create({
       regId,
       participant1Name,
@@ -42,7 +73,6 @@ export const registerUser = async (req, res) => {
       phone,
       events,
     });
-    
 
     res.status(201).json({
       message: "Registration successful",
